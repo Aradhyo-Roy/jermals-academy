8f4ecc553c2670e308ea55c04504db75b33a5000
Aradhyo-Roy <83859573+Aradhyo-Roy@users.noreply.github.com>
2021-06-10 09:42:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function find(){
+    if("textarae")Text==("Aradhyo roy")
+    open(document.getElementById("1").value)
+
+    href= onclick("1");
+
+    document.getElementById("1").innerHTML=href;
+}
